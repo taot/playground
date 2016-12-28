3d61cffe160ad00138e32cda43d686b1b97748f4
Terry Tao <libra_kevin1983@hotmail.com>
2016-12-29 01:09:54 +0800

    Add markdown support to taodoc

--- javascript/taodoc/test-ejs.js
+++ javascript/taodoc/test-ejs.js
@@ -2,14 +2,14 @@ var ejs = require('ejs');
 var yaml = require('js-yaml');
 var fs = require('fs');
 
-var doc = yaml.safeLoad(fs.readFileSync('tests/api-example.yaml', 'utf8'));
+var doc = yaml.safeLoad(fs.readFileSync('exp/api-example.yaml', 'utf8'));
 console.log(doc.title);
-ejs.renderFile('./tests/template.ejs', doc, {}, function(err, str){
+ejs.renderFile('./exp/template.ejs', doc, {}, function(err, str){
     if (err) {
         console.error(err);
         return;
     }
-    fs.writeFile('./tests/output.html', str, function(err) {
+    fs.writeFile('./exp/output.html', str, function(err) {
         if (err) {
             console.error(err);
             return;
