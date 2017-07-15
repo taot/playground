dca6d7856e4b84b0231f3dba39ec79292e83cfd8
taot <libra_kevin1983@hotmail.com>
2017-07-15 19:20:48 +0800

    usaco: complete wormhole

--- usaco/1.3/wormhole.java
+++ usaco/1.3/wormhole.java
@@ -6,55 +6,164 @@ TASK: wormhole
 import java.io.*;
 import java.util.*;
 
-class template {
+class wormhole {
 
     private static String task = "wormhole";
 
     private static int N;
 
+    private static int trapCount = 0;
+
     private static Map<Integer, List<Coord>> rows = new HashMap<Integer, List<Coord>>();
 
     private static Map<Integer, Coord> pointsById = new HashMap<Integer, Coord>();
 
-    public static void main (String [] args) throws IOException {
+    public static void main1(String[] args) {
+        N = 6;
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = i + 1;
+        }
+        permute(arr, 0);
+    }
+
+
+    private static void permute(int[] arr, int idx) {
+        if (idx >= arr.length) {
+            // printArr(arr);
+            boolean trapped = checkTrap(arr);
+            if (trapped) {
+                printArr(arr);
+                trapCount++;
+            }
+            return;
+        }
+
+        for (int i = 0; i <= idx; i++) {
+            swap(arr, idx, i);
+            permute(arr, idx + 2);
+            swap(arr, idx, i);
+        }
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    private static void printArr(int[] arr) {
+        boolean first = true;
+        for (int i = 0; i < arr.length; i++) {
+            if (first) {
+                first = false;
+            } else {
+                // System.out.print(',');
+            }
+            // System.out.print(arr[i]);
+        }
+        // System.out.println();
+    }
+
+    private static boolean checkTrap(int[] arr) {
+        setupPairs(arr);
+        for (Coord p : pointsById.values()) {
+            boolean t = walk(p);
+            if (t) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String [] args) throws IOException {
         BufferedReader f = new BufferedReader(new FileReader(task + ".in"));
         PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(task + ".out")));
         StringTokenizer st = new StringTokenizer(f.readLine());
         N = Integer.parseInt(st.nextToken());
         Coord[] holes = new Coord[N];
-        int id = 0;
+
         for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(f.readLine());
+            st = new StringTokenizer(f.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            Coord c = new Coord(i, x, y);
+            int id = i + 1;
+            Coord c = new Coord(id, x, y);
             insertToRows(c);
-            pointsById.put(i, c);
+            pointsById.put(id, c);
         }
 
+        preprocess();
 
+        for (int row : rows.keySet()) {
+            List<Coord> list = rows.get(row);
+            // System.out.println("row " + row);
+            // System.out.println(list);
+        }
 
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = i + 1;
+        }
+        permute(arr, 0);
 
+        // System.out.println(trapCount);
+        out.println(trapCount);
 
-        out.println(i1+i2);
         out.close();
     }
 
-    
+    private static void setupPairs(int[] arr) {
+        for (int i = 0; i < N / 2; i++) {
+            Coord p1 = pointsById.get(arr[i * 2]);
+            Coord p2 = pointsById.get(arr[i * 2 + 1]);
+            p1.pair = p2;
+            p2.pair = p1;
+        }
+    }
 
     /**
      * If trapped, return true
      */
     private static boolean walk(Coord start) {
         BitSet visited = new BitSet();
+        Map<Integer, Integer> visitCount = new HashMap<Integer, Integer>();
+        boolean transported = false;
         Coord p = start;
-        visited.set(p.id);
-        p = p.next;
-        while (p != null && ! visited.get(p.id)) {
-            p = p.next;
-        }
+
+        do {
+            visited.set(p.id);
+            incCount(visitCount, p.id);
+            if (transported) {
+                p = p.next;
+            } else {
+                p = p.pair;
+            }
+            transported = ! transported;
+
+        } while (p != null && getVisitCount(visitCount, p.id) <= 2);
+        // } while (p != null && (! visited.get(p.id) || transported));
+
         boolean trapped = p != null;
         return trapped;
+    }
+
+    private static int getVisitCount(Map<Integer, Integer> visitCount, int id) {
+        Integer i = visitCount.get(id);
+        if (i == null) {
+            return 0;
+        } else {
+            return i;
+        }
+    }
+
+    private static void incCount(Map<Integer, Integer> visitCount, int id) {
+        Integer i = visitCount.get(id);
+        if (i == null) {
+            visitCount.put(id, 1);
+        } else {
+            visitCount.put(id, i + 1);
+        }
     }
 
     private static void insertToRows(Coord c) {
@@ -66,7 +175,7 @@ class template {
         row.add(c);
     }
 
-    private static preprocess() {
+    private static void preprocess() {
         for (List<Coord> row : rows.values()) {
             // sort by x
             Collections.sort(row, new Comparator<Coord>() {
@@ -91,6 +200,9 @@ class template {
             this.id = id;
             this.x = x;
             this.y = y;
+        }
+        public String toString() {
+            return String.format("%d (%d, %d)", id, x, y);
         }
     }
 }
