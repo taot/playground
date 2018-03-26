65286dac15f9c21492c0cb404bce59f39ba5fd4e
taot <libra_kevin1983@hotmail.com>
2018-03-26 10:38:56 +0800

    Adding computer-systems files

---
+++ computer-systems/ch02/show_bytes.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+typedef unsigned char *byte_pointer;
+
+void show_bytes(byte_pointer start, size_t len) {
+    size_t i;
+    for (i = 0; i < len; i++) {
+        printf(" %.2x", start[i]);
+    }
+    printf("\n");
+}
+
+void show_int(int x) {
+    show_bytes((byte_pointer) &x, sizeof(int));
+}
+
+void show_float(float x) {
+    show_bytes((byte_pointer) &x, sizeof(float));
+}
+
+void show_pointer(void *x) {
+    show_bytes((byte_pointer) &x, sizeof(void *));
+}
+
+int main(int argc, char *argv[]) {
+    show_int(100);
+    show_float(5.8);
+    int x = 10;
+    show_pointer(&x);
+    return 0;
+}
